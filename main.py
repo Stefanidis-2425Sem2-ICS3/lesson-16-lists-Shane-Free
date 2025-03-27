f3a0d69bb00a5521170ca25efd7b25ba2c226109
Shane-Free <frees@students.dsbn.org>
2025-03-27 11:57:19 -0400

    lists

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+#shane f
+#lists
+#March 25/25
+#this code creates 100 numbers and averages them all
+import random
+import math
+def list(): return random.randint(1,100)
+bunchofnumbers = [list() for i in range (100) ]
+print(bunchofnumbers)
+average = sum(bunchofnumbers) / 100
+print(average)
